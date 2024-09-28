784eba36e183343638ea425d682420316bbc82ff
MD Danish Shadab <mddanishshadab8@gmail.com>
2024-09-29 04:20:09 +0530

    First Commit

--- src/main/java/com/myblog7/repository/PostRepository.java
+++ src/main/java/com/myblog7/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.myblog7.repository;
 
 import com.myblog7.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
